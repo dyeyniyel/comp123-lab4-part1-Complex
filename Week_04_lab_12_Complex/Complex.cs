2bf4d67b9c7fdc70ccda9abc67ef195f18a99026
Janiel Mark Javier <jjavie27@my.centennialcollege.ca>
2024-04-20 17:07:48 -0400

    Add project files.

---
+++ Week_04_lab_12_Complex/Complex.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_04_lab_12_Complex
+{
+    public class Complex
+    {
+        //properties of Complex class
+        public int Real { get; }
+        public int Imaginary { get; }
+        public double Argument => Math.Atan2(Imaginary, Real); //calculated property. equivalent to tan^(-1)⁡〖(Imaginary⁄Real)〗]. 
+        public double Modulus => Math.Sqrt(Math.Pow(Real, 2) + Math.Pow(Imaginary, 2)); //calculated property. Equivalent to √(〖Real〗^2+〖Imaginary〗^2 )]. 
+        public static Complex Zero { get; } = new Complex(0, 0); //factory property to create a complex number with both real and imaginary parts set to 0.
+
+        //constructor
+        public Complex(int real = 0, int imaginary = 0)
+        {
+            Real = real;
+            Imaginary = imaginary;
+        }
+        //returns the object as ordered pairs
+        public override string ToString()
+        {
+            return $"({Real},{Imaginary})";
+        }
+
+        //Operator Overloading for adding the two numbers
+        public static Complex operator +(Complex lhs, Complex rhs)
+        {
+            int real = lhs.Real + rhs.Real;
+            int imaginary = lhs.Imaginary + rhs.Imaginary;
+            return new Complex(real, imaginary);
+        }
+        //Operator Overloading for subtracting the two numbers
+        public static Complex operator -(Complex lhs, Complex rhs)
+        {
+            int real = lhs.Real - rhs.Real;
+            int imaginary = lhs.Imaginary - rhs.Imaginary;
+            return new Complex(real, imaginary);
+        }
+        //Operator Overloading for equal equal operator
+        public static bool operator ==(Complex lhs, Complex rhs)
+        {
+            bool realEqualTo = lhs.Real == rhs.Real;
+            bool imaginaryEqualTo = lhs.Imaginary == rhs.Imaginary;
+            return realEqualTo && imaginaryEqualTo;
+        }
+        //Operator Overloading for not equal operator
+        public static bool operator !=(Complex lhs, Complex rhs)
+        {
+            bool isEqual = lhs == rhs;
+            return !isEqual;
+        }
+
+        //additional tasks
+
+        //operator overloading for multiplication.  <a, b> * <c, d> = <ac-bd, ad+bc>. 
+        public static Complex operator *(Complex lhs, Complex rhs)
+        {
+            int real = (lhs.Real * rhs.Real) - (lhs.Imaginary * rhs.Imaginary); //ac-bd
+            int imaginary = (lhs.Real * rhs.Imaginary) + (lhs.Imaginary * rhs.Real); //ad+bc
+            return new Complex(real, imaginary);
+        }
+        //Operator Overloading for unary - operator.  -<a, b> = <-a, -b>
+        public static Complex operator -(Complex complexUnary)
+        {
+            return new Complex(-complexUnary.Real, -complexUnary.Imaginary);
+        }
+    }
+}
---
+++ Week_04_lab_12_Complex/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_04_lab_12_Complex
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex c0 = new Complex(-2, 3);
+            Complex c1 = new Complex(-2, 3);
+            Complex c2 = new Complex(1, -2);
+
+            Console.WriteLine("Given numbers:");
+            Console.WriteLine($"Let c0 be {c0}");
+            Console.WriteLine($"Let c1 be {c1}");
+            Console.WriteLine($"Let c2 be {c2}");
+
+
+            Console.WriteLine("\nAdding c1 and c2:");
+            Console.WriteLine($"{c1} + {c2} = {c1 + c2}");
+
+            Console.WriteLine("\nSubstracting c1 and c2:");
+            Console.WriteLine($"{c1} - {c2} = {c1 - c2}");
+
+            Complex c3 = c1 + c2; 
+            Complex c4 = c1 * c2; //declaring variable for multiplication
+            Complex c5 = -c1; //declaring variable for unary
+
+
+            Console.WriteLine($"\n{c3} in polar form is {c3.Modulus:f2}cis({c3.Argument:f2})");
+
+            Console.WriteLine("\nShowing that c0 is equal to c1:");
+            Console.WriteLine($"{c0} {(c0 == c1 ? "=" : "!=")} {c1}");
+
+            Console.WriteLine("\nShowing that c0 is not equal to c2:");
+            Console.WriteLine($"{c0} {(c0 == c2 ? "=" : "!=")} {c2}");
+
+
+            //Display Multiplication of two complex numbers
+            Console.WriteLine("\nMultiplying c1 and c2:");
+            Console.WriteLine($"{c1} * {c2} = {c4}");
+
+            //Display overloading of Unary operator
+            Console.WriteLine("\nChanging the sign of the operand using unary (-) operator:");
+            Console.WriteLine($"Negated complex number: -{c0} = {c5}");
+
+            Console.ReadLine();
+        }
+    }
+}
